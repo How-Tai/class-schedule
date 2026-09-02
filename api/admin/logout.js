c795f9f3449169a6681f8c7da089fbab7aae3ea1
Tai <taisamrit12@gmail.com>
2026-09-02 18:39:58 +0700

    Add admin logout API

---
+++ api/admin/logout.js
@@ -0,0 +1,9 @@
+module.exports = function handler(req, res) {
+	if(req.method !== "POST") {
+		res.setHeader("Allow", "POST");
+		return res.status(405).json({ error: "Method not allowed" });
+	}
+
+	res.setHeader("Set-Cookie", "admin_session=; HttpOnly; Secure; SameSite=Strict; Path=/; Max-Age=0");
+	return res.status(200).json({ ok: true });
+};
